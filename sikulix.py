982c8f8bfa178a19c92d30bf45c8bfa5aeaba821
Željko Filipin <zeljko.filipin@gmail.com>
2020-01-26 13:46:39 +0100

    Go to settings > guitar > challenges > history
    Also move files to a folder.

--- sikulix.py
+++
@@ -1,2 +0,0 @@
-click(Location(600, 400))
-wheel(WHEEL_UP, 100)
---
+++ sikulix/sikulix.py
@@ -0,0 +1,11 @@
+# settings - guitar
+click("settings.png")
+click("guitar.png")
+
+# challenges - history
+click("challenges.png")
+click("history.png")
+
+# scroll down
+click(Location(700, 400))
+wheel(WHEEL_UP, 100)
