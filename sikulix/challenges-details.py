1a0ee0e8400bc8b87ab1aeaa86418e3797ebc8be
Željko Filipin <zeljko.filipin@gmail.com>
2020-01-29 15:59:38 -0800

    sikulix: challenge songs and levels
    OCR doesn't work for levels :(

--- sikulix/challenges-details.py
+++ sikulix/challenges-details.py
@@ -11,6 +11,8 @@ for x in range(0, 1): # update range as needed
     click(Location(700, 400)) # first challenge on the page
     wait(1)
     print(Region(100,100,1266,100).text()) # challenge title
+    print(Region(457,231,647,516).text()) # songs
+    print(Region(1260,250,80,500).text()) # levels
     click("back.png")
     wait("history.png")
     mouseMove(Location(700, 400)) # first challenge on the page
