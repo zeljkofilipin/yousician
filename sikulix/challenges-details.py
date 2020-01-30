0702fb02330821e53e71bccf46c8c4840cb644b5
Željko Filipin <zeljko.filipin@gmail.com>
2020-01-29 16:07:20 -0800

    sikulix: fix song levels for a challenge
    Get each level separately to fix OCR.

--- sikulix/challenges-details.py
+++ sikulix/challenges-details.py
@@ -7,12 +7,16 @@ click("challenges.png")
 wait("history.png")
 click("history.png")
 wait(1)
-for x in range(0, 1): # update range as needed
+for x in range(0, 5): # update range as needed
     click(Location(700, 400)) # first challenge on the page
     wait(1)
     print(Region(100,100,1266,100).text()) # challenge title
     print(Region(457,231,647,516).text()) # songs
-    print(Region(1260,250,80,500).text()) # levels
+    # levels
+    print(Region(1285,270,25,30).text())
+    print(Region(1285,405,25,30).text())
+    print(Region(1285,545,25,30).text())
+    print(Region(1285,685,25,30).text())
     click("back.png")
     wait("history.png")
     mouseMove(Location(700, 400)) # first challenge on the page
